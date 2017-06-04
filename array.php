918ef6ffb8aeba74c2fd253874f0131b302e566f
Zoli <analog007@gmail.com>
2017-06-05 01:01:24 +0200

    Pages

--- array.php
+++ array.php
@@ -8,37 +8,37 @@ $categories = array(
         "elements"=> array(
             array(
                 "category_id"=>1,
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"",
                 "pic_location"=>"img/q1-1.jpg"
             ),
             array (
                 "category_id"=>2,
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"",
                 "pic_location"=>"img/q1-2.jpg"
             ),
             array (
                 "category_id"=>"3",
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"",
                 "pic_location"=>"img/q1-3.jpg"
             ),
             array (
                 "category_id"=>"4",
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"",
                 "pic_location"=>"img/q1-4.jpg"
             ),
             array (
                 "category_id"=>"5",
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"",
                 "pic_location"=>"img/q1-5.jpg"
             ),
             array (
                 "category_id"=>"6",
-                "description"=>"greenpeace",
+                "description"=>"Greenpeace",
                 "subtitle"=>"I prefer humans",
                 "pic_location"=>"img/q1-6.jpg"
             )
--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ if(!el.parents('.question').data('select-multiple')){
     if(!el.hasClass('selected')) {
         el.parents('.question').find('.selected').removeClass('selected').find('i').toggleClass('fa-check-square-o fa-square-o');
         el.addClass('selected').find('i').toggleClass('fa-square-o fa-check-square-o');
-    } else {
-
     }
 } else {
     if(!el.hasClass('selected')) {
